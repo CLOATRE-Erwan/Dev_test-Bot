035c83be3b8a8f8fb1dfd015f2083e546950218b
CLOATREErwan <erwan.cloatre@isen-ouest.yncrea.fr>
2020-12-16 19:22:15 +0000

    hello message

--- main.py
+++ main.py
@@ -13,7 +13,8 @@ async def on_message(message):
   if message.author == client.user:
     return
 
-
+  if message.content.startswith('!hello'):
+    await message.channel.send('Hello !')
 
 
 client.run(os.getenv('token'))
