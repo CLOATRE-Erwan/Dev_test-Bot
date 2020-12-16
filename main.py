8a20a20747a20f1df031968a7be2343072861830
CLOATREErwan <erwan.cloatre@isen-ouest.yncrea.fr>
2020-12-16 20:16:45 +0000

    test command bot

--- main.py
+++ main.py
@@ -1,29 +1,47 @@
 import discord
+from discord.ext import commands
 import os
+# import matplotlib.pyplot as plt
+# import numpy as np
 
 client = discord.Client()
+bot = commands.Bot(command_prefix='$')
 
 @client.event
 async def on_ready():
   print('Bot logged in as {0.user}'.format(client))
 
 
-@client.event
-async def on_message(message):
-  if message.author == client.user:
-    return
+# @client.event
+# async def on_message(message):
+#   if message.author == client.user:
+#     return
 
 
-  if message.content.startswith('!model'):
-    await message.channel.send('```python\ndef model(X, theta):\n  return X.dot(theta)```')
+#   if message.content.startswith('!model'):
+#     await message.channel.send('```python\ndef model(X, theta):\n  return X.dot(theta)```')
 
-  if message.content.startswith('!cost_function'):
-    await message.channel.send('```python\ndef cost_function(X, y, theta):\n  m =l(y)\n return 1/(2*m) * np.sum((model(X, theta) - y)**2)```')
+#   if message.content.startswith('!cost_function'):
+#     await message.channel.send('```python\ndef cost_function(X, y, theta):\n  m =l(y)\n return 1/(2*m) * np.sum((model(X, theta) - y)**2)```')
     
-  if message.content.startswith('!grad'):
-    await message.channel.send('```python\ndef cost_function(X, y, theta):\n  m =l(y)\n return 1/(2*m) * np.sum((model(X, theta) - y)**2)```')
+#   if message.content.startswith('!grad'):
+#     await message.channel.send('```python\ndef cost_function(X, y, theta):\n  m =l(y)\n return 1/(2*m) * np.sum((model(X, theta) - y)**2)```')
 
-  if message.content.startswith('!gradient_descent'):
-    await message.channel.send('```python\ndef gradient_descent(X, y, theta, learning_rate, n_iterations):\n for i in range(0, n_iterations):\n  theta = theta - learning_rate * grad(X, y, theta)\n return theta```')
+#   if message.content.startswith('!gradient_descent'):
+#     await message.channel.send('```python\ndef gradient_descent(X, y, theta, learning_rate, n_iterations):\n for i in range(0, n_iterations):\n  theta = theta - learning_rate * grad(X, y, theta)\n return theta```')
+
+  # if message.content.startswith('!plt'):
+  #   x = np.random.rand(3)
+  #   y = np.random.rand(3)
+  #   plt.plot(x, y)
+  #   plt.title(f'{message.author}\'s Graph')
+  #   plt.savefig(fname='plot')
+  #   await message.channel.send(file=discord.File('plot.png'))
+  #   os.remove('plot.png')
+
+@bot.command()
+async def test(ctx, arg):
+    await ctx.send(arg)
+    
 
 client.run(os.getenv('token'))
