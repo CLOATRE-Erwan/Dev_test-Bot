5e3f6449a469f82e63294717842729415c911426
CLOATREErwan <erwan.cloatre@isen-ouest.yncrea.fr>
2020-12-16 21:13:20 +0000

    bot command plot

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import discord
 from discord.ext import commands
 import os
-# import matplotlib.pyplot as plt
-# import numpy as np
+import matplotlib.pyplot as plt
+import numpy as np
 
-client = discord.Client()
+# client = discord.Client()
 bot = commands.Bot(command_prefix='$')
 
-@client.event
-async def on_ready():
-  print('Bot logged in as {0.user}'.format(client))
+# @client.event
+# async def on_ready():
+#   print('Bot logged in as {0.user}'.format(client))
 
 
 # @client.event
@@ -30,18 +30,25 @@ async def on_ready():
 #   if message.content.startswith('!gradient_descent'):
 #     await message.channel.send('```python\ndef gradient_descent(X, y, theta, learning_rate, n_iterations):\n for i in range(0, n_iterations):\n  theta = theta - learning_rate * grad(X, y, theta)\n return theta```')
 
-  # if message.content.startswith('!plt'):
-  #   x = np.random.rand(3)
-  #   y = np.random.rand(3)
-  #   plt.plot(x, y)
-  #   plt.title(f'{message.author}\'s Graph')
-  #   plt.savefig(fname='plot')
-  #   await message.channel.send(file=discord.File('plot.png'))
-  #   os.remove('plot.png')
+#   if message.content.startswith('!plt'):
+#     x = np.random.rand(3)
+#     y = np.random.rand(3)
+#     plt.plot(x, y)
+#     plt.title(f'{message.author}\'s Graph')
+#     plt.savefig(fname='plot')
+#     await message.channel.send(file=discord.File('plot.png'))
+#     os.remove('plot.png')
 
 @bot.command()
-async def test(ctx, arg):
-    await ctx.send(arg)
+async def plot(ctx, arg):
+    x = np.random.rand(int(arg))
+    y = np.random.rand(int(arg))
+    plt.plot(x, y)
+    plt.title(f'{ctx.message.author}\'s Graph')
+    plt.savefig(fname='plot')
+    await ctx.send(file=discord.File('plot.png'))
+    os.remove('plot.png')
     
 
-client.run(os.getenv('token'))
+# client.run(os.getenv('token'))
+bot.run(os.getenv('token'))
